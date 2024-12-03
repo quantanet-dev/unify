c659a99dc4512e3478af4e3fb2d5527418cb2dee
Quantanet.Dev <quantanet.dev@gmail.com>
2024-12-03 13:31:43 -0500

    Added logging system;

--- Unify/src/Unify.h
+++ Unify/src/Unify.h
@@ -3,6 +3,7 @@
 // ONLY FOR USE BY UNIFY APPLICATIONS
 
 #include "Unify/Application.h"
+#include "Unify/Log.h"
 
 // ------ Entry Point ------
 #include "Unify/EntryPoint.h"
--- Unify/src/Unify/EntryPoint.h
+++ Unify/src/Unify/EntryPoint.h
@@ -6,6 +6,10 @@ extern Unify::Application* Unify::CreateApplication();
 
 int main(int argc, char** argv) {
 
+	Unify::Log::Init();
+	UN_CORE_WARN("Initialized Log!");
+	UN_INFO("Hello Unify!");
+
 	auto app = Unify::CreateApplication();
 	app->Run();
 	delete app;
